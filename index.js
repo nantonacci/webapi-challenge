ae6d1deeab21cdbae596227191b70cd2278d6406
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-11-09 09:16:31 -0800

    Structure complete.

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const server = require('./server.js');
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
---
+++ router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const Action = require('./data/helpers/actionModel.js');
+const Project = require('./data/helpers/projectModel.js');
+const router = express.Router();
+
+module.exports = router;
